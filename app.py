6d7afe66cb663b8f6f1d580d68b3204ed5e232da
diana <dianajewell@Dianas-MacBook-Air.local>
2022-01-23 19:36:04 -0500

    updated to add user login signup logout

--- app.py
+++ app.py
@@ -1,15 +1,18 @@
-from flask import Flask, render_template, request, flash, redirect
+from flask import Flask, render_template, request, flash, redirect, session, g
 from flask_debugtoolbar import DebugToolbarExtension
 import requests
 # from sqlalchemy import desc
-# from sqlalchemy.exc import IntegrityError
+from sqlalchemy.exc import IntegrityError
+from datetime import date
 # import os
 # from werkzeug.utils import secure_filename
 
-# from forms import PropAndHeadpieceForm, ProductionForm, RoleForm
-from models import db, connect_db
-# from user_models import User
+from forms import NotesForm, UserAddForm, LoginForm
+from models import db, connect_db, User, Note, Card
 
+
+CURR_USER_KEY = "curr_user"
+today = date.today()
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///tarot_app_db'
@@ -32,16 +35,135 @@ API_BASE_URL = "https://rws-cards-api.herokuapp.com/api/v1/cards"
 @app.route('/')
 def display_homepage():
     '''docstring'''
-    return render_template('home.html')
+    return render_template('home.html', today=today)
 
     
 
-@app.route('/get-daily-card')
+@app.route('/get-daily-card', methods=["GET", "POST"])
 def get_card():
-        
+    
+    # if user not logged in, this shouldn't work and redirect to login page
+
+    # if a card has already been selected + saved for today, this should redirect to user home page with daily/weekly/monthly overview
+
+
     resp = requests.get(f"{API_BASE_URL}/random?n=1")
     data = resp.json()
     card = data['cards']
     image_url = f"static/cards/{card[0]['name_short']}.jpg"
-    return render_template('daily-card.html', card=card, image_url=image_url)
 
+    form = NotesForm()
+
+    if form.validate_on_submit():
+        note = Note(
+            text=form.notes.data,
+            user_id=g.user.id
+        )
+        card = Card(
+            card_name = card[0]['name_short'],
+            user_id = g.user.id
+        )
+
+        db.session.add(note)
+        db.session.add(card)
+        db.session.commit()
+
+        return redirect("/")
+
+    else:
+        return render_template('daily-card.html', card=card, image_url=image_url, today=today, form=form)
+
+
+
+##############################################################################
+# User signup/login/logout
+
+
+@app.before_request
+def add_user_to_g():
+    """If we're logged in, add curr user to Flask global."""
+
+    if CURR_USER_KEY in session:
+        g.user = User.query.get(session[CURR_USER_KEY])
+
+    else:
+        g.user = None
+
+
+def do_login(user):
+    """Log in user."""
+
+    session[CURR_USER_KEY] = user.id
+
+
+def do_logout():
+    """Logout user."""
+
+    if CURR_USER_KEY in session:
+        del session[CURR_USER_KEY]
+
+
+@app.route('/signup', methods=["GET", "POST"])
+def signup():
+    """Handle user signup.
+
+    Create new user and add to DB. Redirect to home page.
+
+    If form not valid, present form.
+
+    If the there already is a user with that username: flash message
+    and re-present form.
+    """
+
+    form = UserAddForm()
+
+    if form.validate_on_submit():
+        try:
+            user = User.signup(
+                username=form.username.data,
+                password=form.password.data,
+                email=form.email.data,
+                image_url=form.image_url.data or User.image_url.default.arg,
+            )
+            db.session.commit()
+
+        except IntegrityError:
+            flash("Username already taken", 'danger')
+            return render_template('signup.html', form=form)
+
+        do_login(user)
+
+        return redirect("/")
+
+    else:
+        return render_template('signup.html', form=form)
+
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    """Handle user login."""
+
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        user = User.authenticate(form.username.data,
+                                 form.password.data)
+
+        if user:
+            do_login(user)
+            flash(f"Hello, {user.username}!", "success")
+            return redirect("/")
+
+        flash("Invalid credentials.", 'danger')
+
+    return render_template('login.html', form=form)
+
+
+@app.route('/logout')
+def logout():
+    """Handle logout of user."""
+
+    if CURR_USER_KEY in session:
+        del session[CURR_USER_KEY]
+        flash(f"You have successfully logged out!", "success")
+    return redirect('/login')
--- models.py
+++ models.py
@@ -1,27 +1,152 @@
 """SQLAlchemy models for Tarot App DB."""
 
+from datetime import datetime
+
+from flask_bcrypt import Bcrypt
 from flask_sqlalchemy import SQLAlchemy
 
+bcrypt = Bcrypt()
 db = SQLAlchemy()
 
 
-class Test(db.Model):
-    """docstring"""
+class User(db.Model):
+    """User in the system."""
 
-    __tablename__ = 'tests'
+    __tablename__ = 'users'
 
     id = db.Column(
         db.Integer,
         primary_key=True,
     )
 
-    name = db.Column(
+    email = db.Column(
         db.Text,
         nullable=False,
         unique=True,
     )
 
+    username = db.Column(
+        db.Text,
+        nullable=False,
+        unique=True,
+    )
 
+    image_url = db.Column(
+        db.Text,
+        default="/static/images/default-pic.png",
+    )
+
+    password = db.Column(
+        db.Text,
+        nullable=False,
+    )
+
+    notes = db.relationship('Note')
+
+    cards = db.relationship('Card')
+
+
+    def __repr__(self):
+        return f"<User #{self.id}: {self.username}, {self.email}>"
+
+    @classmethod
+    def signup(cls, username, email, password, image_url):
+        """Sign up user.
+
+        Hashes password and adds user to system.
+        """
+
+        hashed_pwd = bcrypt.generate_password_hash(password).decode('UTF-8')
+
+        user = User(
+            username=username,
+            email=email,
+            password=hashed_pwd,
+            image_url=image_url,
+        )
+
+        db.session.add(user)
+        return user
+
+    @classmethod
+    def authenticate(cls, username, password):
+        """Find user with `username` and `password`.
+
+        This is a class method (call it on the class, not an individual user.)
+        It searches for a user whose password hash matches this password
+        and, if it finds such a user, returns that user object.
+
+        If can't find matching user (or if password is wrong), returns False.
+        """
+
+        user = cls.query.filter_by(username=username).first()
+
+        if user:
+            is_auth = bcrypt.check_password_hash(user.password, password)
+            if is_auth:
+                return user
+
+        return False
+
+
+class Note(db.Model):
+    """A users notes on individual cards"""
+
+    __tablename__ = 'notes'
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True,
+    )
+
+    text = db.Column(
+        db.Text,
+        nullable=False,
+    )
+
+    timestamp = db.Column(
+        db.DateTime,
+        nullable=False,
+        default=datetime.utcnow(),
+    )
+
+    user_id = db.Column(
+        db.Integer,
+        db.ForeignKey('users.id', ondelete='CASCADE'),
+        nullable=False,
+    )
+
+    user = db.relationship('User')
+
+
+class Card(db.Model):
+    """A users daily card"""
+
+    __tablename__ = 'cards'
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True,
+    )
+
+    card_name = db.Column(
+        db.String(10),
+        nullable=False,
+    )
+
+    timestamp = db.Column(
+        db.DateTime,
+        nullable=False,
+        default=datetime.utcnow(),
+    )
+
+    user_id = db.Column(
+        db.Integer,
+        db.ForeignKey('users.id', ondelete='CASCADE'),
+        nullable=False,
+    )
+
+    user = db.relationship('User')
 
 
 def connect_db(app):

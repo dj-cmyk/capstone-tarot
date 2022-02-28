92be0b12af170a0ee357538020d12ef82c4896b7
diana <dianajewell@Dianas-MacBook-Air.local>
2022-02-28 11:22:49 -0500

    try again for db url heroku issue

--- app.py
+++ app.py
@@ -13,12 +13,11 @@ from models import db, connect_db, User, Card
 
 
 app = Flask(__name__)
-uri = os.getenv("DATABASE_URL")  # or other relevant config var
+uri = os.environ.get("DATABASE_URL")  
 if uri.startswith("postgres://"):
     uri = uri.replace("postgres://", "postgresql://", 1)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = (
-    os.environ.get(uri, 'postgresql:///tarot_app_db'))
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(uri, 'postgresql:///tarot_app_db')
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
